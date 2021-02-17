f1f06c322545398bb270d1733a82143fad598212
Thaumy Cheng <Studio_ai@outlook.com>
2021-02-17 23:02:46 +0800

    ~~

--- NightCat/page.js
+++ NightCat/page.js
@@ -1,16 +1,16 @@
 ﻿function set_dark() {
-	var divs = document.getElementsByTagName("*")
+	var divs = document.getElementsByTagName(new RegExp('^[script]'))
 
 	for (var i = 0; i < divs.length; i++) {
 		var el = divs[i];
 		if (el.childElementCount > 1) {
 			if (el.parentNode.tagName == "BODY") {
-				el.classList.add('NightCat')
+				el.classList.add('NightCat_normal')
 			}
 			if (el.tagName == "LI") {
 				el.classList.add('NightCat_li')
 			} else {
-				el.classList.add('NightCat')
+				el.classList.add('NightCat_normal')
 			}
 		} else {
 			el.classList.add('NightCat_child')
@@ -20,9 +20,9 @@
 
 function set_white() {
 	var divs = document.getElementsByTagName("*")
-
+	var reg = new RegExp('NightCat_[A-Za-z0-9]*');
 	for (var i = 0; i < divs.length; i++) {
-		divs[i].classList.remove('NightCat')
-		divs[i].classList.remove('NightCat_child')
+		var el = divs[i];
+		el.className = el.className.replace(reg, ' ');
 	}
 }
