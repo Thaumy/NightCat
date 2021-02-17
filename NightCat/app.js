5154b3ffd3ea734c8c931b572cbc2a1c8e28c091
Thaumy Cheng <Studio_ai@outlook.com>
2021-02-17 13:20:48 +0800

    icon

---
+++ NightCat/app.js
@@ -0,0 +1,22 @@
+﻿new Vue({
+    el: '#app',
+    vuetify: new Vuetify({
+        theme: {
+            dark: true
+        }
+    }),
+    data() {
+        return {
+            UUID_N: '',
+            UUID_D: '',
+            YMD_XXXX: '',
+            YMD: '',
+            RndNumber: '',
+            RndString: '',
+            PubK: '',
+            PriK: '',
+            Plain: '',
+            Cipher: ''
+        }
+    },
+})
